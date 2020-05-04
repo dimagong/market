f3bd6a51a345d29f6af991720a0aeb50358cc7e3
dimagong <dimagong@gmail.com>
2020-05-04 17:01:48 +0300

    Updates

---
+++ precache-manifest.fb6f4f53f804ff4fcc84ca8bc4c21310.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "133394cfbfa6528b7067585e3db67507",
+    "url": "/market/index.html"
+  },
+  {
+    "revision": "7bea05a2f74eff0e26d5",
+    "url": "/market/static/css/2.d788185a.chunk.css"
+  },
+  {
+    "revision": "b4edbb8e56def855379a",
+    "url": "/market/static/css/main.e09d8638.chunk.css"
+  },
+  {
+    "revision": "7bea05a2f74eff0e26d5",
+    "url": "/market/static/js/2.3ccfc128.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/market/static/js/2.3ccfc128.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4edbb8e56def855379a",
+    "url": "/market/static/js/main.fd32e1fa.chunk.js"
+  },
+  {
+    "revision": "9e5fd4a7a2d2532f9f93",
+    "url": "/market/static/js/runtime-main.96be51a2.js"
+  },
+  {
+    "revision": "19ef7b17bc9414c63c7781251ce0d24d",
+    "url": "/market/static/media/crown.19ef7b17.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/market/static/media/shopping-bag.30f07071.svg"
+  }
+]);
